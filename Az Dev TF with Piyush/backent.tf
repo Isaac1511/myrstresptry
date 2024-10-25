ceb1c256f84736633f94f6b64b5077262709f8bd
Isaac1511 <46512840+Isaac1511@users.noreply.github.com>
2024-10-25 07:21:23 -0400

    Delete Az Dev TF with Piyush directory

--- Az Dev TF with Piyush/backent.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Multi_use_resource_group"
-    storage_account_name = "storagedev010"                      
-    container_name       = "stodev-tainer"                       
-    key                  = "prod.terraform.tfstate"        
-  }
-}
-
